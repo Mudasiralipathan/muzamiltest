f4de74fcd9b4fcf103a0e4541830a1ab12ce16b9
mudasir <mapppali1108@gmail.com>
2024-07-15 03:47:57 -0700

    muzamiltest

---
+++ src/hero/Hero.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './hero.css'
+
+const Hero = () => {
+  return (
+    <section className="services">
+    <div className="services-row">
+        <div className="service-item">
+            <h3>Web Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <a href="#" className="btn">Learn More</a>
+        </div>
+        <div className="service-item">
+            <h3>Mobile App Development</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <a href="#" className="btn">Learn More</a>
+        </div>
+        <div className="service-item">
+            <h3>Graphic Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <a href="#" className="btn">Learn More</a>
+        </div>
+        <div className="service-item">
+            <h3>Digital Marketing</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <a href="#" className="btn">Learn More</a>
+        </div>
+    </div>
+    <div className="services-row">
+        <div className="service-item">
+            <h3>SEO Services</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <a href="#" className="btn">Learn More</a>
+        </div>
+        <div className="service-item">
+            <h3>Content Writing</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <a href="#" className="btn">Learn More</a>
+        </div>
+        <div className="service-item">
+            <h3>E-commerce Solutions</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <a href="#" className="btn">Learn More</a>
+        </div>
+        <div className="service-item">
+            <h3>UI/UX Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+            <a href="#" className="btn">Learn More</a>
+        </div>
+    </div>
+</section>
+
+  )
+}
+
+export default Hero
